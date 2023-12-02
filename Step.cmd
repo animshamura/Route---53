5bb856aa9f9156e6b7e2325136c0756ba362749f
Shamura Ahmad Anim <47661479+animshamura@users.noreply.github.com>
2023-12-03 00:28:42 +0600

    Create Step.cmd

---
+++ Step.cmd
@@ -0,0 +1,4 @@
+# Step 1 : Register a domain in any suitable hosting site. <br/> 
+# Step 2 : Create hosted zone with the registered domain. <br/> 
+# Step 3 : Create A record with any type of entry. It can be load balancer or any ip address. <br/> 
+# Step 4 : Check the registered record. <br/> 
